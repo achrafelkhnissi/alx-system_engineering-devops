938b23e004d7f6d12d7e8eb689d1bf926f6d3d43
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2024-01-16 22:01:55 +0100

    .

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module for task 1"""
+"""Module for task 0"""
 
 
 def top_ten(subreddit):
@@ -7,12 +7,11 @@ def top_ten(subreddit):
     of the subreddit"""
     import requests
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subreddit),
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
     if sub_info.status_code >= 300:
-        print('None')
-    else:
-        [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
+        return 0
+
+    return sub_info.json().get("data").get("subscribers")
