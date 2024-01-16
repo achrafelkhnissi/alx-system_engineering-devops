8bf83d37b9d2a0b3c7e9697f8e20c372051b29e7
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2024-01-16 22:09:05 +0100

    .

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,17 +2,19 @@
 
 """Module for task 0"""
 
+import requests
+
 
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
+    for a given subreddit. If an invalid subreddit is given, the function
+    returns 0."""
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
+    sub_info = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
+
     if sub_info.status_code >= 300:
         return 0
 
-    return sub_info.json().geot("data").get("subscribers")
+    return sub_info.json().get("data").get("subscribers")
